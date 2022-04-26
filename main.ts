56a2bd94fe5720cdf681eb4cbe4115620e687917
Haoqi Shan Xu <104140318+al-7555056@users.noreply.github.com>
2022-04-26 08:29:18 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
 let strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
+strip.showColor(neopixel.colors(NeoPixelColors.Green))
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) > 4) {
-        music.playMelody("C5 C5 C5 C5 C5 C5 C5 C5 ", 500)
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+        maqueen.motorStop(maqueen.Motors.All)
+        if (randint(0, 1) == 1) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Red))
+            music.playMelody("C5 C5 C5 C5 C5 C5 C5 C5 ", 500)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+            basic.pause(1000)
+            strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        }
         if (randint(0, 1) == 0) {
             maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
             maqueen.motorStop(maqueen.Motors.M2)
@@ -14,5 +22,7 @@ basic.forever(function () {
         basic.pause(1000)
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
     }
 })
